0416fa9234c8c6d7f1c7f293594029aa3a9c78a1
Therakar <matteo.pau200@gmail.com>
2023-07-19 12:45:41 +0200

    toggle AddFriendForm funtionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,23 @@ import FriendsList from "./FriendsList";
 import FormAddFriends from "./FormAddFriend";
 import FormSplitBill from "./FormSplitBill";
 import Button from "./Button";
+import { useState } from "react";
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriends />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriends />}
+        <Button onClick={handleShowAddFriend}>
+          {!showAddFriend ? "Add friend" : "Close"}
+        </Button>
       </div>
 
       <FormSplitBill />
